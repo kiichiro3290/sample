29da82baee90a3250df49929a4647360ceb3d0f8
kiichiro3290 <kai.kiichiro.418@s.kyushu-u.ac.jp>
2023-07-17 03:23:41 +0900

    create dist dir

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,19 @@ import { resolve } from 'path'
 import { defineConfig } from 'vite'
 
 const root = resolve(__dirname, 'src')
+const outDir = resolve(__dirname, 'dist')
 
 export default defineConfig({
     base: '/',
     root,
+    build: {
+        outDir,
+        rollupOptions: {
+            input: {
+                index: resolve(root, 'index.html'),
+                threejs: resolve(root, 'threejs', 'index.html'),
+            }
+        }
+    }
 })
 
