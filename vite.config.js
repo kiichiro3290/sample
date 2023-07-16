ac24e6d542df30dd4cd1b36fe057ca9041f6e602
kiichiro3290 <kai.kiichiro.418@s.kyushu-u.ac.jp>
2023-07-17 05:44:37 +0900

    resolve path

--- vite.config.js
+++ vite.config.js
@@ -5,17 +5,10 @@ const root = resolve(__dirname, 'src')
 const outDir = resolve(__dirname, 'dist')
 
 export default defineConfig({
-    base: '/sample/',
+    base: process.env.GITHUB_PAGES
+        ? "sample"
+        : "./",  
     root,
-    build: {
-        outDir,
-        rollupOptions: {
-            input: {
-                index: resolve(root, 'index.html'),
-                threejs: resolve(root, 'threejs', 'index.html'),
-            }
-        }
-    },
     resolve: {
         alias: {
             '@': root
