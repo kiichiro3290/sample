8f6fd26af3adcb141d1e3a7005e1761173ae10c0
kiichiro3290 <kai.kiichiro.418@s.kyushu-u.ac.jp>
2023-07-17 05:44:56 +0900

    remove

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,6 @@ import { resolve } from 'path'
 import { defineConfig } from 'vite'
 
 const root = resolve(__dirname, 'src')
-const outDir = resolve(__dirname, 'dist')
 
 export default defineConfig({
     base: process.env.GITHUB_PAGES
